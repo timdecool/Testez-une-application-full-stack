eb6ce2fcf159162c2d462d76927af0d8f4f2d9d5
timdecool <tim.decool@gmail.com>
2026-05-14 15:47:57 +0200

    logout test suite

---
+++ front/cypress/e2e/logout.cy.ts
@@ -0,0 +1,20 @@
+﻿
+describe('Logout spec', () => {
+
+  it('should logout and redirect to home page', () => {
+    cy.login();
+    cy.get('span').contains('Logout').click();
+    cy.url().should('include', '/').should('not.include', '/sessions');
+  });
+
+  it('should redirect to login when visting protected pages while not logged', () => {
+    cy.visit('/sessions');
+    cy.url().should('not.include', '/sessions').should('include', '/login');
+
+    cy.visit('/me');
+    cy.url().should('not.include', '/me').should('include', '/login');
+
+    cy.visit('/sessions/create');
+    cy.url().should('not.include', '/sessions/create').should('include', '/login');
+  });
+})
